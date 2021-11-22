80d1808bf8e666bb18a922a1503688b98c5d508d
Allllakis <allakiselova20@gmail.com>
2021-11-22 19:32:07 +0300

    add meetupdetail component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f005f65d40b22d8f"; }
+/******/ 		__webpack_require__.h = function() { return "d450fa52067449d7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/e/ee/Sagrada_Familia_01.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="A First Meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
